9a2ad96ae6e01c95d407b9004b177914a5d10ff3
Branko Sabo <wolfplayer98@gmail.com>
2019-01-07 01:24:44 +0100

    fuck everything

--- public/includes/navigation.php
+++ public/includes/navigation.php
@@ -1,6 +1,6 @@
 <header id="header">
     <nav class="navbar navbar-expand-lg navbar-light navbar-fixed-top" id="mainNav">
-        <div class="container"> <a href="index"><img src="images/logo.png" alt="logo"></a>
+        <div class="container"> <a href="index"><img src="images/TwinKnives.png" alt="logo"></a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"> <i class="fa fa-bars"></i> </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto sf-js-enabled sf-arrows">
