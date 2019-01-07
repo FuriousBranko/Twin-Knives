62ee0099baa7976875a894f778714252983751b9
bandoxx <radivoje.pupovac98@gmail.com>
2019-01-07 02:10:27 +0100

    something

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,40 +1,25 @@
-<?php
-    // if(isset($_POST['submit'])) {
-    //     if($_POST['username'] == 'admin' && $_POST['password'] == 'admin') {
-    //         $_SESSION['admin'] = true;
-    //         header("Location: index.php");
-    //     } else {
-    //         $_SESSION['admin'] = false;
-    //         header("Location: index.php");
-    //     }
-    // } else {
-    //     $_SESSION['admin'] = false;
-    // }
-?>
 <html lang="en">
-    <head>
-    <?php
+<?php
     $title = "Admin Panel";
     $keywords = "Something, very, interesting, about, this, hackaton";
 	$description = "Something very interesting";
 	$authors = "Radivoje Pupovac & Branko Sabo";
     require_once '../includes/headData.php';
 ?>
-    </head>
-    <body>
-<div class="reservation-form-inner">
-						<h3>Login</h3>
-						<form>
-							<div class="form-group">
-								<input type="text" class="form-control" id="username" placeholder="Admin Meister">
-							</div>
-							<div class="form-group">
-								<input type="password" class="form-control" id="password" placeholder="Password">
-							</div>
-							<div class="form-btn text-center">
-								<a href="#" class="btn btn-info">Login</a><br>
-							</div>
-						</form>
-					</div>
+<body>
+    <div class="reservation-form-inner">
+    	<h3>Login</h3>
+    	<form>
+    		<div class="form-group">
+    			<input type="text" class="form-control" id="username" placeholder="Admin Meister">
+    		</div>
+    		<div class="form-group">
+    			<input type="password" class="form-control" id="password" placeholder="Password">
+    		</div>
+    		<div class="form-btn text-center">
+    			<a href="#" class="btn btn-info">Login</a><br>
+    		</div>
+    	</form>
+    </div>
 </body>
 </html>
--- public/login.php
+++ public/login.php
@@ -11,19 +11,19 @@
 
 <body>
 <div class="reservation-form-inner">
-						<h3>Login</h3>
-						<form>
-							<div class="form-group">
-								<input type="email" class="form-control" id="email" placeholder="Email">
-							</div>
-							<div class="form-group">
-								<input type="password" class="form-control" id="password" placeholder="Password">
-							</div>
-							<div class="form-btn text-center">
-								<a href="#" class="btn btn-info">Login</a><br>
-                                <a href="#">Forgotten Password</a>
-							</div>
-						</form>
-					</div>
+	<h3>Login</h3>
+	<form>
+		<div class="form-group">
+			<input type="email" class="form-control" id="email" placeholder="Email">
+		</div>
+		<div class="form-group">
+			<input type="password" class="form-control" id="password" placeholder="Password">
+		</div>
+		<div class="form-btn text-center">
+			<a href="#" class="btn btn-info">Login</a><br>
+            <a href="#">Forgotten Password</a>
+		</div>
+	</form>
+</div>
 </body>
 </html>
